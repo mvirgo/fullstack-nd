70599abd865d1a0a8506a2c60065ed981721e4bc
mvirgo <mvirgs44@gmail.com>
2019-04-29 15:39:58 -0700

    feat: styling for main catalog and links back to it

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -11,14 +11,14 @@ Base = declarative_base()
 
 class Category(Base):
     __tablename__ = 'category'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
     # Function to be able to send JSON objects in a serializable format
     @property
     def serialize(self):
-       
+
        return {
            'id'   : self.id,
            'name' : self.name,
