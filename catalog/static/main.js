ac515c19c304eb609d0d22b2fe6940b990e0d902
mvirgo <mvirgs44@gmail.com>
2019-05-03 13:55:17 -0700

    Docs: comments for JS scripts

--- catalog/static/main.js
+++ catalog/static/main.js
@@ -1,4 +1,8 @@
 function init() {
+  /*
+  Shows or hides Google Sign-In and authentication-related messages
+  when page loads based on whether they are signed in or not.
+  */
   gapi.load('auth2');
   if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
     document.getElementById("googleOut").style.display="block";
@@ -12,6 +16,11 @@ function init() {
 }
 
 function onSignIn(googleUser) {
+  /*
+  Shows sign out link and content for only authenticated users,
+  while hiding the Google Sign In button and any content
+  that mentions the need to sign in.
+  */
   document.getElementById("googleOut").style.display="block";
   document.getElementById("googleIn").style.display="none";
   document.getElementById("authNeeded").style.display="block";
@@ -19,6 +28,12 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
+  /*
+  Signs out the user, then displays the Google Sign In button,
+  along with any messages discussing the need to sign in
+  for any related features. Also, hides the sign out link,
+  as well as hiding any features that require authentication.
+  */
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
